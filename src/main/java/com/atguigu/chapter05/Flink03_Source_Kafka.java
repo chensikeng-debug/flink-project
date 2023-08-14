4d99376afe1d0ad6d40eb13ee371fb7b83ac8b8e
chensikneg <18665908221@163.com>
2023-08-14 11:00:34 +0800

    feat:kafkasource

--- src/main/java/com/atguigu/chapter05/Flink03_Source_Kafka.java
+++ src/main/java/com/atguigu/chapter05/Flink03_Source_Kafka.java
@@ -1,4 +1,27 @@
 package com.atguigu.chapter05;
 
+import lombok.SneakyThrows;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+
+import java.util.Properties;
+
 public class Flink03_Source_Kafka {
+    @SneakyThrows
+    public static void main(String[] args) {
+        // 0.Kafka相关配置
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "hadoop162:9092,hadoop163:9092,hadoop164:9092");
+        properties.setProperty("group.id", "Flink01_Source_Kafka");
+        properties.setProperty("auto.offset.reset", "latest");
+
+        // 1. 创建执行环境
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env
+                .addSource(new FlinkKafkaConsumer<>("sensor", new SimpleStringSchema(), properties))
+                .print("kafka source");
+
+        env.execute();
+    }
 }
